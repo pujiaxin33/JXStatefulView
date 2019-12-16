e1acdd34f84adc6f0c79cb3391db04fb987e79d8
pjx <pujx@jingbo360.com>
2019-12-16 09:58:37 +0800

    Initial Commit

---
+++ JXStatefulView/JXStatefulView.h
@@ -0,0 +1,19 @@
+//
+//  JXStatefulView.h
+//  JXStatefulView
+//
+//  Created by jiaxin on 2019/12/16.
+//  Copyright © 2019 jiaxin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JXStatefulView.
+FOUNDATION_EXPORT double JXStatefulViewVersionNumber;
+
+//! Project version string for JXStatefulView.
+FOUNDATION_EXPORT const unsigned char JXStatefulViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JXStatefulView/PublicHeader.h>
+
+
